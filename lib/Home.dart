72c64f666c470d322db3f5d8dee8a1e4b22f9fce
Aftab ahmad <aftabamin38@gmail.com>
2023-09-24 23:16:38 +0500

    adding images_icon.dart, orientation.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,5 @@
-// home.dart
 import 'package:flutter/material.dart';
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -7,54 +7,64 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
-  // Order to Save
   Order _order = Order(quantity: 0);
+
   String? _validateItemRequired(String? value) {
     return value?.isEmpty == true ? 'Item Required' : null;
   }
 
   String? _validateItemCount(String? value) {
-    int? _valueAsInteger = value != null && value.isNotEmpty ? int.tryParse(value) : null;
-    return _valueAsInteger == null || _valueAsInteger == 0 ? 'At least one Item is Required' : null;
+    int? _valueAsInteger =
+    value != null && value.isNotEmpty ? int.tryParse(value) : null;
+    return _valueAsInteger == null || _valueAsInteger == 0
+        ? 'At least one Item is Required'
+        : null;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: SafeArea(child:
-      Column(
-        children: <Widget>[
-          Form(
-            key: _formStateKey,
-            //autovalidate: true,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'Espresso',
-                      labelText: 'Item',
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formStateKey,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Espresso',
+                        labelText: 'Item',
+                      ),
+                      validator: (value) => _validateItemRequired(value),
+                      onSaved: (value) => _order.item = value,
                     ),
-                    validator: (value) => _validateItemRequired(value),
-                    onSaved: (value) => _order.item = value,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: '3',
-                      labelText: 'Quantity',
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: '3',
+                        labelText: 'Quantity',
+                      ),
+                      validator: (value) => _validateItemCount(value),
+                      onSaved: (value) {
+                        _order.quantity = int.tryParse(value ?? '0') ?? 0;
+                      },
                     ),
-                    validator: (value) => _validateItemCount(value),
-                    //onSaved: (value) => _order.quantity = int.tryParse(value),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      )),
+            ElevatedButton(
+              onPressed: _submitOrder,
+              child: Text('Submit Order'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -66,20 +76,6 @@ class _HomeState extends State<Home> {
       print('Order Quantity: ${_order.quantity}');
     }
   }
-
-
-}
-
-
-String? _validateItemRequired(String value) {
-  return value.isEmpty ? 'Item Required' : null;
-}
-
-
-String? _validateItemCount(String value) {
-  // Check if value is not null and convert to integer
-  int? _valueAsInteger = value.isEmpty ? 0 : int.tryParse(value);
-  return _valueAsInteger == 0 ? 'At least one Item is Required' : null;
 }
 
 class Order {
@@ -88,6 +84,3 @@ class Order {
 
   Order({this.item, required this.quantity});
 }
-
-
-
